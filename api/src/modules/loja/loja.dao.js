a2a33307eec15f37d95f0848113d5c8347616aec
Puntar <thiagopuntar@gmail.com>
2021-01-31 23:27:33 -0300

    fix(cardapio): listProducts

--- api/src/modules/loja/loja.dao.js
+++ api/src/modules/loja/loja.dao.js
@@ -11,6 +11,7 @@ module.exports = class LojaDao extends (
   async listProducts() {
     return this.db("products")
       .where("isLoja", true)
+      .andWhere("isActive", true)
       .select("id", "title", "description", "unit", "price");
   }
 
