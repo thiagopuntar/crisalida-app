505466fe7793a0d5a4f981812c5b0e936f4e1636
Puntar <thiagopuntar@gmail.com>
2021-01-31 21:51:17 -0300

    chore(rateLimit)

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const compression = require("compression");
 const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const apiRoutes = require("./infra/routes/api");
 const reportRoutes = require("./infra/routes/reports");
 const appRoute = require("./infra/routes/app");
@@ -22,7 +23,12 @@ app.use(morgan("combined"));
 app.use(compression());
 app.use(helmet());
 
-app.use("/api/v1/cardapio", cardapioRoute);
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 60,
+});
+
+app.use("/api/v1/cardapio", limiter, cardapioRoute);
 app.use("/api/v1/login", loginRoute);
 app.use("/api/v1", authMiddleware, apiRoutes);
 app.use("/reports", authMiddleware, reportRoutes);
