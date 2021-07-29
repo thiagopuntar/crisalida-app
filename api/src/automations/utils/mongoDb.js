f965f89894c43afc00da2f99af13200e846f8f03
Thiago Puntar <thiagopuntar@gmail.com>
2021-07-29 19:18:31 -0300

    remove console.log from mongoDB connection

--- api/src/automations/utils/mongoDb.js
+++ api/src/automations/utils/mongoDb.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { MONGODB_CONN_STRING } = process.env;
-console.log('mongo connection', MONGODB_CONN_STRING);
 
 mongoose.connect(MONGODB_CONN_STRING, {
   useNewUrlParser: true,
